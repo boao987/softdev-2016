24f3348ef1490febdb8445211524469a5c5b92a1
boao987 <schooloffranklin12@gmail.com>
2016-04-08 17:16:01 +0000

    hw09

---
+++ hw/hw09/hw09.py
@@ -0,0 +1,27 @@
+import time
+
+def nameArgLogger(f):
+    def inner(*arg):
+        print f.func_name + ": " + str(arg)
+        return f(*arg)
+    return inner
+
+def execTime(f):
+    def inner(*arg):
+        startTime = time.time()
+        f(*arg)
+        endTime = time.time()
+        
+        return endTime-startTime
+    return inner
+    
+    
+#@nameArgLogger
+@execTime
+def fib(n):
+    if (n <= 2):
+        return 1
+    else:
+        return fib(n-2) + fib(n-1)
+
+print fib(5)
